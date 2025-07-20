e4b8986341d771d16a7aded9a6a218c9dd5abea9
nithesh1997 <nitheshroshan007@gmail.com>
2025-07-20 09:49:51 +0530

    added tanstack optional fetures

--- src/components/ReactQueryFetch.jsx
+++ src/components/ReactQueryFetch.jsx
@@ -9,10 +9,17 @@ async function fetchPosts() {
 }
 
 const ReactQueryFetch = () => {
-  const { data, isLoading, isError, error } = useQuery({
+  const { data, isLoading, isFetching, isError, error } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts, 
+    //********* */ Optional configuration options: *****************************
+    // staleTime: 5000,  // ⏱️ Data is considered "fresh" for 5 seconds
+     // After 5 sec, data becomes "stale" and may refetch on remount/focus
+    //  refetchInterval: 1000,        // 🔁 Automatically refetch data every 1 second (1000 ms)
+    // Even if data is fresh, it will refetch in background every second
+    // refetchIntervalInBackground: true, // ✅ Refetch even when tab is not focused
   });
+
 
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error: {error.message}</p>;
